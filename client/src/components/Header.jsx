c11c330e26d9fd7585417267e3ec047ec049e9ac
Anand Sharma <84133329+anand777sharma@users.noreply.github.com>
2024-02-10 21:18:00 +0530

    ...

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
    
     <header>
-        <h1>TODO list</h1>
+        <h1>Lets do it...</h1>
     </header>
   )
 }
--- client/src/components/Progress.jsx
+++ client/src/components/Progress.jsx
@@ -29,35 +29,45 @@ const Progress = () => {
 
     return (
         <>
+        {getTodos()>0 ?(<>
+        
             <div className="" style={{width:400}}>
 
-                <CircularProgressbar
-                    value={percentage}
-                    text={`${percentage}%`}
-                    styles={buildStyles({
-                        // Rotation of path and trail, in number of turns (0-1)
-                        rotation: 0.25,
+<CircularProgressbar
+    value={percentage}
+    text={`${percentage}%`}
+    styles={buildStyles({
+        // Rotation of path and trail, in number of turns (0-1)
+        rotation: 0.25,
 
-                        // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
-                        strokeLinecap: 'butt',
+        // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+        strokeLinecap: 'butt',
 
-                        // Text size
-                        textSize: '16px',
+        // Text size
+        textSize: '16px',
 
-                        // How long animation takes to go from one percentage to another, in seconds
-                        pathTransitionDuration: 1.5,
+        // How long animation takes to go from one percentage to another, in seconds
+        pathTransitionDuration: 1.5,
 
-                        // Can specify path transition in more detail, or remove it entirely
-                        // pathTransition: 'none',
+        // Can specify path transition in more detail, or remove it entirely
+        // pathTransition: 'none',
 
-                        // Colors
-                        pathColor: `rgba(62, 152, 199)`,
-                        textColor: '#f88',
-                        trailColor: '#d6d6d6',
-                        backgroundColor: '#3e98c7',
-                    })}
-                />
-            </div>
+        // Colors
+        pathColor: `rgba(62, 152, 199)`,
+        textColor: '#f88',
+        trailColor: '#d6d6d6',
+        backgroundColor: '#3e98c7',
+    })}
+/>
+</div>
+        
+        </>):(<>
+        <p className="display-6">
+            Start your learning journey
+        </p>
+        </>)}
+
+          
 
         </>
     )
